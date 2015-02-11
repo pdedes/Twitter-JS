97bc1236143c0592180a81db1af451fe24f6d0b5
Max Kayen <mkayen@gmail.com>
2015-02-10 21:32:04 -0500

    Left off at Seeding tweetBank

---
+++ tweetBank.js
@@ -0,0 +1,17 @@
+var _ = require('underscore');
+
+var data = []
+
+var add = function(name, text){
+	data.push({ name: name, text: text })
+}
+
+var list = function() {
+	return _.clone(data);
+}
+
+var find = function(properties){
+	return _.where(data, properties);
+}
+
+module.exports = { add: add, list: list, find: find };
