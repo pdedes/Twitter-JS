6d134fdbece61c3cc36b53288c80a55d017a2d3a
Max Kayen <mkayen@gmail.com>
2015-02-10 20:51:56 -0500

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var logger =  require('morgan');
+
+var app = express();
+
+app.use(logger('dev'))
+
+app.get('/', function (request, response){
+	response.send('hello world!')
+})
+
+app.get('/news', function (request, response){
+	response.send('Todays News')
+})
+
+var server = app.listen(3000, function(){
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Exanple app listening at http://%s:%s', host, port)
+})
